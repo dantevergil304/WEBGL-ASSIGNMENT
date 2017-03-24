56d98a2bfbd147fc72cf16e78ab270a738769519
Dang Vu Minh Hieu <protopro03@gmail.com>
2017-03-24 14:35:18 +0700

    Commit Date : 24/03/2017

---
+++ Wall.js
@@ -0,0 +1,230 @@
+var wallVertexBuffer;
+var wallTextureBuffer;
+var wallIndexBuffer;
+var wallHeight = 0.125;
+var wallSize = 0.125;
+var wallWidth = 0.125;
+function initWallBuffer(){
+	
+  wallVertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, wallVertexBuffer);
+
+	var verts =  [
+	  //Font face
+	    -FlatWidth, -wallHeight,  wallSize,
+       FlatWidth, -wallHeight,  wallSize,
+       FlatWidth,  wallHeight,  wallSize,
+      -FlatWidth,  wallHeight,  wallSize,
+
+      // Back face
+      -FlatWidth,  wallHeight,  wallSize,
+      -FlatWidth,  wallHeight, -wallSize,
+       FlatWidth,  wallHeight, -wallSize,
+       FlatWidth, -wallHeight, -wallSize,
+
+      // Top face
+      -FlatWidth,  wallHeight, -wallSize,
+      -FlatWidth,  wallHeight,  wallSize,
+       FlatWidth,  wallHeight,  wallSize,
+       FlatWidth,  wallHeight, -wallSize,
+
+      // Bottom face
+      -FlatWidth, -wallHeight, -wallSize,
+       FlatWidth, -wallHeight, -wallSize,
+       FlatWidth, -wallHeight,  wallSize,
+      -FlatWidth, -wallHeight,  wallSize,
+
+      // Right face
+       FlatWidth, -wallHeight, -wallSize,
+       FlatWidth,  wallHeight, -wallSize,
+       FlatWidth,  wallHeight,  wallSize,
+       FlatWidth, -wallHeight,  wallSize,
+
+      // Left face
+      -FlatWidth, -wallHeight, -wallSize,
+      -FlatWidth, -wallHeight,  wallSize,
+      -FlatWidth,  wallHeight,  wallSize,
+      -FlatWidth,  wallHeight, -wallSize,
+    ];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+	wallVertexBuffer.itemSize = 3;
+	wallVertexBuffer.numItems = 24;
+
+	wallTextureBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, wallTextureBuffer);
+
+	var textureCoords = [
+    	// Front face
+      0.0, 0.0,
+      1.0, 0.0,
+      1.0, 1.0,
+      0.0, 1.0,
+
+      // Back face
+      1.0, 0.0,
+      1.0, 1.0,
+      0.0, 1.0,
+      0.0, 0.0,
+
+      // Top face
+      0.0, 1.0,
+      0.0, 0.0,
+      1.0, 0.0,
+      1.0, 1.0,
+
+      // Bottom face
+      1.0, 1.0,
+      0.0, 1.0,
+      0.0, 0.0,
+      1.0, 0.0,
+
+      // Right face
+      1.0, 0.0,
+      1.0, 1.0,
+      0.0, 1.0,
+      0.0, 0.0,
+
+      // Left face
+      0.0, 0.0,
+      1.0, 0.0,
+      1.0, 1.0,
+      0.0, 1.0,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    wallTextureBuffer.itemSize = 2;
+    wallTextureBuffer.numItems = 24;
+
+    wallIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, wallIndexBuffer);
+
+     var indexs = [
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
+    ];
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexs), gl.STATIC_DRAW);
+    wallIndexBuffer.itemSize = 1;
+    wallIndexBuffer.numItems = 36;
+}
+
+
+function Wall(x, y){
+	this.posX = x;
+	this.posY = y;
+  this.width = wallWidth;
+  this.height = wallHeight;
+  this.size = wallSize;
+}
+
+
+var wallTexture;
+function initWallTexture(){
+  wallTexture = gl.createTexture();
+  wallTexture.image = new Image();
+  wallTexture.image.onload = function(){
+    handleLoadedTexture(wallTexture);
+  }
+  wallTexture.image.src="mud.gif";
+}
+
+
+Wall.prototype.draw = function(){
+	mvPushMatrix();
+
+	mat4.translate(mvMatrix, [this.posX, this.posY, cubeSize - 1]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, wallVertexBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, wallVertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, wallTextureBuffer);
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, wallTextureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, wallTexture);
+	gl.uniform1i(shaderProgram.samplerUniform, 0);
+
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, wallIndexBuffer);
+	setUniformMatrix();
+	gl.drawElements(gl.TRIANGLES, wallIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0)
+
+	mvPopMatrix();
+}	
+
+var wall2VertexBuffer;
+function initWall2Buffer(){
+  wall2VertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, wall2VertexBuffer);
+  var verts =  [
+    //Font face
+      -wallWidth, -FlatHeight - 2*wallHeight,  wallSize,
+       wallWidth, -FlatHeight - 2*wallHeight,  wallSize,
+       wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+      -wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+
+      // Back face
+      -wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+      -wallWidth,  FlatHeight + 2*wallHeight, -wallSize,
+       wallWidth,  FlatHeight + 2*wallHeight, -wallSize,
+       wallWidth, -FlatHeight - 2*wallHeight, -wallSize,  
+
+      // Top face
+      -wallWidth,  FlatHeight + 2*wallHeight, -wallSize,
+      -wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+       wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+       wallWidth,  FlatHeight + 2*wallHeight, -wallSize,
+
+      // Bottom face
+      -wallWidth, -FlatHeight - 2*wallHeight, -wallSize,
+       wallWidth, -FlatHeight - 2*wallHeight, -wallSize,
+       wallWidth, -wallHeight - 2*wallHeight,  wallSize,
+      -wallWidth, -wallHeight - 2*wallHeight,  wallSize,
+
+      // Right face
+       wallWidth, -FlatHeight - 2*wallHeight, -wallSize,
+       wallWidth,  FlatHeight + 2*wallHeight, -wallSize,
+       wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+       wallWidth, -FlatHeight - 2*wallHeight,  wallSize,
+
+      // Left face
+      -wallWidth, -FlatHeight - 2*wallHeight, -wallSize,
+      -wallWidth, -FlatHeight - 2*wallHeight,  wallSize,
+      -wallWidth,  FlatHeight + 2*wallHeight,  wallSize,
+      -wallWidth,  FlatHeight + 2*wallHeight, -wallSize,
+    ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+  wall2VertexBuffer.itemSize = 3;
+  wall2VertexBuffer.numItems = 24;
+}
+
+function Wall2(x, y){
+  Wall.call(this, x, y);
+}
+
+Wall2.prototype = Object.create(Wall.prototype);
+
+Wall2.prototype.draw = function (){
+  mvPushMatrix();
+
+  mat4.translate(mvMatrix, [this.posX, this.posY, cubeSize - 1]);
+  gl.bindBuffer(gl.ARRAY_BUFFER, wall2VertexBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, wall2VertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, wallTextureBuffer);
+  gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, wallTextureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, wallTexture);
+  gl.uniform1i(shaderProgram.samplerUniform, 0);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, wallIndexBuffer);
+  setUniformMatrix();
+  gl.drawElements(gl.TRIANGLES, wallIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0)
+
+  mvPopMatrix();
+}
